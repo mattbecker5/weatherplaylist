7ac16982ec209add9f0d501c25180cdc66ae95e2
matthew becker <mattbecker7@gmail.com>
2020-04-10 00:23:11 -0400

    matt

--- weather-playlist/src/app/small-calendar/small-calendar.component.ts
+++ weather-playlist/src/app/small-calendar/small-calendar.component.ts
@@ -13,11 +13,13 @@ export class SmallCalendarComponent implements OnInit {
   constructor() { }
 
   public currentCalendarDays: CalendarDay[];
+  public monthBufferDays: CalendarDay[];
 
   ngOnInit(): void {
 
     //app state
     let currentCalendar: CalendarMonth;
+    let tempBufferDays = [];
     let smallCalendarCurrent = {};
     let largeCalendarCurrent = {};
     let events = {};
@@ -52,11 +54,50 @@ export class SmallCalendarComponent implements OnInit {
             currentMonth.days.push(day);
         };
 
+        
+
         return currentMonth;
     };
 
     //the start of the app in time
     currentCalendar = createMonthArray(4, 2020);
+
+    let numBufferDays = 0;
+
+    //this is needed to put empty spaces in begging of each month
+    if(currentCalendar.days[0].day === "Monday"){
+        numBufferDays = 1;
+    };
+
+    if(currentCalendar.days[0].day === "Tuesday"){
+        numBufferDays = 2;
+    };
+
+    if(currentCalendar.days[0].day === "Wednesday"){
+        numBufferDays = 3;
+    };
+
+    if(currentCalendar.days[0].day === "Thursday"){
+        numBufferDays = 4;
+    };
+
+    if(currentCalendar.days[0].day === "Friday"){
+        numBufferDays = 5;
+    };
+
+    if(currentCalendar.days[0].day === "Saturday"){
+        numBufferDays = 6;
+    };
+
+    for (let i = 0; i < numBufferDays; i++) {
+        let day = new CalendarDay(0, "last month");
+        console.log(day);
+        tempBufferDays.push(day);
+        //this.monthBufferDays.push(day);
+    };
+
+    this.monthBufferDays = tempBufferDays;
+
     this.currentCalendarDays = currentCalendar.days;
     console.log(currentCalendar);
     console.log(this.currentCalendarDays);
