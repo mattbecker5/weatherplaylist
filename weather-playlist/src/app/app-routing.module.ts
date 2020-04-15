26fbac2a6375d47cb80befec2a223a592fdfd84b
kk60 <kk60@students.uwf.edu>
2020-04-15 12:41:34 +0800

    kk

--- weather-playlist/src/app/app-routing.module.ts
+++ weather-playlist/src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path:'', redirectTo:'/home', pathMatch:'full' },
-  { path: '**', redirectTo: '/home' }
+  // { path:'', redirectTo:'/home', pathMatch:'full' },
+  // { path: '**', redirectTo: '/home' }
   // { path:'profile', component: }
 ];
 
