e4974f3590385abe2f995bff2c02bbbf9f78b346
kk60 <kk60@students.uwf.edu>
2020-04-15 08:45:26 +0800

    KK

---
+++ weather-playlist/src/app/components/functionality/functionality.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-functionality',
+  templateUrl: './functionality.component.html',
+  styleUrls: ['./functionality.component.scss']
+})
+export class FunctionalityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
