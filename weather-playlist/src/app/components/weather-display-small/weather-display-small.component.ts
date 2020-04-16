fb849b56763c8fbaaeda702258ed49d851712a1d
matthew becker <mattbecker7@gmail.com>
2020-04-15 22:12:35 -0400

    matt

--- weather-playlist/src/app/components/weather-display-small/weather-display-small.component.ts
+++ weather-playlist/src/app/components/weather-display-small/weather-display-small.component.ts
@@ -10,6 +10,20 @@ export class WeatherDisplaySmallComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var x = document.getElementById("demo");
+
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+    }
+
+    function showPosition(position) {
+      x.innerHTML = "Latitude: " + position.coords.latitude + 
+      "<br>Longitude: " + position.coords.longitude;
+    }
   }
 
 }
