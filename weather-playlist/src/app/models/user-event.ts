2e54b6071e93505f172f1fcfb2708f92041f59bf
kk60 <kk60@students.uwf.edu>
2020-04-22 14:56:36 +0800

    kk

--- weather-playlist/src/app/models/user-event.ts
+++ weather-playlist/src/app/models/user-event.ts
@@ -7,6 +7,7 @@ export class UserEvent {
     title: String;
     startTime: String;
     endTime: String;
+    uid: String;
 
     // constructor(year:String, month:String, day:String, dayLong:String, type:String, title:String, startTime:String, endTime:String){
     constructor(event){
@@ -18,5 +19,6 @@ export class UserEvent {
         this.title = event.title;
         this.startTime = event.startTime;
         this.endTime = event.endTime;
+        this.uid = event.uid;
     }
 }
