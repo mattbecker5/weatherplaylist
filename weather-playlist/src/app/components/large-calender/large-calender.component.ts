ab674a422dac724e452abdb6171a7a22ef5e0a90
matthew becker <mattbecker7@gmail.com>
2020-04-22 03:20:15 -0400

    matt

--- weather-playlist/src/app/components/large-calender/large-calender.component.ts
+++ weather-playlist/src/app/components/large-calender/large-calender.component.ts
@@ -5,6 +5,7 @@ import { CalendarMonth } from 'src/app/models/calendar-month';
 import { CalendarDay } from 'src/app/models/calendar-day';
 import { CreateCalendarService } from 'src/app/services/create-calendar.service';
 import { Observable } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-large-calender',
@@ -21,7 +22,7 @@ export class LargeCalenderComponent implements OnInit {
 
   
 
-  constructor(private createEventService: CreateEventService, private createCalendar: CreateCalendarService) { }
+  constructor(private createEventService: CreateEventService, private createCalendar: CreateCalendarService, public userService: UserService) { }
 
   ngOnInit(): void {
 
@@ -39,34 +40,33 @@ export class LargeCalenderComponent implements OnInit {
     // this.calendarDays = currentMonth.days;
 
     //getting the saved events -- soon will change to firebase database
-    this.createEventService.getAllEvents().subscribe(events => {
-      console.log("trying to populate");
-    //populating the large calendar with month
-      let currentMonth: CalendarMonth = this.createCalendar.getNewMonth(4,2020);
-      this.currentEvents = events
-      //console.log(this.currentEvents);
-      
-      for(let i = 0; i < currentMonth.days.length; i++){
-        console.log(this.currentEvents.length);
-        for(let j = 0; j < this.currentEvents.length; j++){
-          
-          //console.log(this.currentEvents[j].day);
-          if(currentMonth.days[i].date.toString() === this.currentEvents[j].day){
-            console.log("found");
-            console.log(this.currentEvents[j]);
-            currentMonth.days[i].events.push(this.currentEvents[j])
-            // this.calendarDays.push(calDay);
+    this.userService.user$.subscribe( data => {
+      this.createEventService.getAllEvents(data.uid).subscribe(events => {
+        console.log("trying to populate");
+      //populating the large calendar with month
+        let currentMonth: CalendarMonth = this.createCalendar.getNewMonth(4,2020);
+        this.currentEvents = events
+        //console.log(this.currentEvents);
+        
+        for(let i = 0; i < currentMonth.days.length; i++){
+          console.log(this.currentEvents.length);
+          for(let j = 0; j < this.currentEvents.length; j++){
+            
+            //console.log(this.currentEvents[j].day);
+            if(currentMonth.days[i].date.toString() === this.currentEvents[j].day){
+              console.log("found");
+              console.log(this.currentEvents[j]);
+              currentMonth.days[i].events.push(this.currentEvents[j])
+              // this.calendarDays.push(calDay);
+            }
           }
         }
-      }
-
-      this.calendarMonth = currentMonth;
-      this.calendarDays = currentMonth.days;
-      // if(this.dayEvent.events.length > 0){
-        
-      // }
-      
+  
+        this.calendarMonth = currentMonth;
+        this.calendarDays = currentMonth.days;
+      });
     });
+
   }
 
 }
