3ee891e3c3758898d34ebd5e27a11de80c53cea1
matthew becker <mattbecker7@gmail.com>
2020-04-24 23:06:23 -0400

    matt

--- weather-playlist/src/app/services/apple-music-new.service.ts
+++ weather-playlist/src/app/services/apple-music-new.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 declare const MusicKit:any;
 
 @Injectable({
@@ -9,8 +12,8 @@ export class AppleMusicNewService {
   private jwtToken = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkwyUzlSNEEyRzMifQ.eyJpYXQiOjE1ODc3NTYyMDUsImV4cCI6MTYwMzMwODIwNSwiaXNzIjoiWkZIQjlVNURIOSJ9.zsGtXrqpSnvd3d4F63jJiNvTWiYrFEN7O8UCr_BXKhQ0impJVzW-T3qsvCagryj8UM8XSBgggNITckmTX08-Dw";
   
 
-  constructor() { 
-
+  constructor(private http: HttpClient) { 
+    this.getGenres();
   }
 
   public getAppleApi(){
@@ -35,21 +38,44 @@ export class AppleMusicNewService {
     let playbackState: MusicKit.PlaybackStates = music.playbackState;
     let player: MusicKit.Player = music.player;
 
+    
+
 
     // api.charts(['1']).then(data => {
     //   console.log('the data'+data);
     // });
    
     let url = 'https://itunes.apple.com/us/album/hamilton-original-broadway-cast-recording/1025210938';
+
     music.setQueue({ url: url }).then(function(queue) {
       // Queue is instantiated and set on music player.
-      queue.play();
+      // queue.play();
+
     });
+
+    // https://api.music.apple.com/v1/catalog/{storefront}/genres
+
+    // let results = music.api.search('james brown', { limit: 2, types: 'artists,albums' });
+    // let results = music.api.search('genres');
+    // console.log(results); 
 
     // Playback Controls
     music.play();
     // music.pause();
     // music.stop();
 
+    api.genre('15').then(reply => {
+      console.log(reply);
+    });
+
+    // api.charts(['rap','country']).then(reply => {
+    //   console.log(reply);
+    // });
+    
+  }
+
+  /** GET songs from the server */
+  public getGenres(){
+    
   }
 }
