2800fd19b0159beaa1f1b9eb5960744e2f89353e
matthew becker <mattbecker7@gmail.com>
2020-12-03 22:55:23 -0500

    file replace

--- weather-playlist/e2e/src/app.po.ts
+++ weather-playlist/e2e/src/app.po.ts
@@ -4,6 +4,7 @@ export class AppPage {
   navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
+  
 
   getTitleText(): Promise<string> {
     return element(by.css('app-root .content span')).getText() as Promise<string>;
